45f49837c0f0d14124b2530c53b533317df47a73
Julian Yap <julianokyap@gmail.com>
2017-09-19 13:43:07 -1000

    Suppress embedded links

--- textcmd/text.go
+++ textcmd/text.go
@@ -7,19 +7,18 @@ func Commands(command string) *string {
 
 	switch command {
 	case "!ann":
-		message = "`BitcoinTalk ANN:` https://bitcointalk.org/index.php?topic=1763606.0\n"
+		message = "`BitcoinTalk ANN:` <https://bitcointalk.org/index.php?topic=1763606.0>\n"
 	case "!commands":
 		message = "Here are some custom Ubiq Slack commands\n"
 		message += "\n"
 		message += "*General*\n"
-		message += "`!lennyface` - Lenny face\n"
 		message += "`!price [TICKER]` - Price look ups\n"
 		message += "\n"
 		message += "*Ubiq specific*\n"
 		message += "`!ubqusd [AMOUNT]` - USD conversion\n"
 		message += "`!urls` - URLs\n"
 	case "!wallet":
-		message = "`Web:` https://pyrus.ubiqsmart.com `Web2:` https://myetherwallet.com `GUI:` https://github.com/ubiq/fusion/releases `CLI:` https://github.com/ubiq/go-ubiq/releases\n"
+		message = "`Web:` <https://pyrus.ubiqsmart.com> `Web2:` <https://myetherwallet.com> `GUI:` <https://github.com/ubiq/fusion/releases> `CLI:` <https://github.com/ubiq/go-ubiq/releases>\n"
 	}
 
 	return &message
