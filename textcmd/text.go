22c5af84e26d255db2ee7fca3cb0a2914e927740
Nathan Bowers <outragedhumangithub@mm.st>
2017-09-19 19:12:46 -0500

    Add Lenny Face and APX commands
    !lennyface and !apx

--- textcmd/text.go
+++ textcmd/text.go
@@ -19,6 +19,10 @@ func Commands(command string) *string {
 		message += "`!urls` - URLs\n"
 	case "!wallet":
 		message = "`Web:` <https://pyrus.ubiqsmart.com> `Web2:` <https://myetherwallet.com> `GUI:` <https://github.com/ubiq/fusion/releases> `CLI:` <https://github.com/ubiq/go-ubiq/releases>\n"
+	case "!lennyface":
+		message = "( ͡° ͜ʖ ͡°)"
+	case "!apx":
+		message = "`Channel:` #apx-ventures      `Website:` apxv.org     `Telegram:` t.me/apxventures     `Roadmap:` https://drive.google.com/file/d/0ByqyVzIU5PtFLXp2UGZPcUFYd1U/view"
 	}
 
 	return &message
