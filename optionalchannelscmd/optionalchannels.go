4989da77dcc17d01069a260831d0c423cf88b6e8
Julian Yap <julianokyap@gmail.com>
2017-12-09 23:30:31 -1000

    Add ubiq-korea

--- optionalchannelscmd/optionalchannels.go
+++ optionalchannelscmd/optionalchannels.go
@@ -9,6 +9,7 @@ import (
 var channels = map[string]string{
 	"china":            "360684832558415873",
 	"decred-stakepool": "360173511538638859",
+	"korea":            "389345996938936320",
 	"music":            "359986790217678848",
 	"nsfw":             "360239464884469760",
 	"russia":           "363038830502215693",
